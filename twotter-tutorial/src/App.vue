07e11e62ee3972a0384b53db50b2449babfd4ca4
wenlinchua-sdm <wenlin.chua@sdm.com.my>
2026-07-17 15:25:20 +0800

    Added Button for follower

---
+++ twotter-tutorial/src/App.vue
@@ -0,0 +1,49 @@
+//HTML
+<template>
+  <div id="app">
+    {{user.username}}
+
+    <strong> Followers : </strong> {{followers}}
+
+    <button @click="followUser">
+        Follow
+    </button>
+
+  </div>
+</template>
+
+//Java
+<script>
+
+export default {
+  name: 'App',
+  data() {
+      return{
+        followers : 0,
+        user : {
+            id: 1,
+            username : 'ChuaWL',
+        }
+      }
+  },
+
+  method : {
+    followUser(){
+        this.followers++
+    }
+  }
+
+}
+</script>
+
+//CSS
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
